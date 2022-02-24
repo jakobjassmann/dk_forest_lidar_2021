2395dc52fbb1c29144079c0a22a673e392b14fe1
= <au634851@uni.au.dk>
2022-02-24 17:20:14 +0100

    Added resampling to Bjerreskov treetype raster prep script as the grid was shifted by 0.9 m.

--- scripts/prep/prep_forest_type.R
+++ scripts/prep/prep_forest_type.R
@@ -33,10 +33,21 @@ writeRaster(bornholm == 3, "data/predictor_data/conif_vs_broadleaf/bornholm_fore
 # Bjerreskov et al. 2021 forest types
 library(terra)
 terraOptions(progress = 1)
+
+# Load raster and create binary masks
 treetype_bjer <- rast("data/predictor_data/treetype/treetype_dk_raw.tif")
 treetype_bjer_con <- treetype_bjer == 1
-writeRaster(treetype_bjer_con, "data/predictor_data/treetype/treetype_bjer_con.tif")
 treetype_bjer_dec <- treetype_bjer == 2
+
+# Load target raster for resampling
+dtm10m <- rast("F:/JakobAssmann/EcoDes-DK_v1.1.0/dtm_10m/dtm_10m.vrt")
+
+# Resample
+treetype_bjer_con <- resample(treetype_bjer_con, dtm10m, method = "near")
+treetype_bjer_dec <- resample(treetype_bjer_dec, dtm10m, method = "near")
+
+# Write out rasters
+writeRaster(treetype_bjer_con, "data/predictor_data/treetype/treetype_bjer_con.tif")
 writeRaster(treetype_bjer_dec, "data/predictor_data/treetype/treetype_bjer_dec.tif")
 
 # EOF
