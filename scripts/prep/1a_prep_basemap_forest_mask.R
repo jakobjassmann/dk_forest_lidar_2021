84f4d320c8f6a44775b389746da803db9c79f683
= <au634851@uni.au.dk>
2022-03-18 12:59:36 +0100

    updated forest mask prep script

--- scripts/prep/1a_prep_basemap_forest_mask.R
+++ scripts/prep/1a_prep_basemap_forest_mask.R
@@ -8,7 +8,14 @@ library(terra)
 sub_tree_cover <- rast("data/basemap_forests/sub_tree_cover_2016.tif")
 forest_mask <- sub_tree_cover == 2
 
+# Project to EcoDes-DK15 grid
 dtm10m <- rast("F:/JakobAssmann/EcoDes-DK15_v1.1.0/dtm_10m/dtm_10m.vrt")
-forest_mask <- project(forest_mask, dtm10m, method = "near")
+forest_mask <- terra::project(forest_mask, dtm10m, method = "near")
+
+# Set 0 to NA
+forest_mask <- classify(forest_mask, 
+                        matrix(c(0, NA, 
+                                 1, 1),
+                               byrow = T, ncol = 2))
 writeRaster(forest_mask,
             filename = "data/basemap_forests/forest_mask.tif")
